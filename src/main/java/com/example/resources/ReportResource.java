c04ccf386ddc838a3b03abfb7b120d0d4c1b7d57
xypeXs <xypexs173@gmail.com>
2022-07-20 12:02:38 +0300

    download xls file is now post request type

--- src/main/java/com/example/resources/ReportResource.java
+++ src/main/java/com/example/resources/ReportResource.java
@@ -3,10 +3,7 @@ package com.example.resources;
 import com.example.services.ReportService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
@@ -24,8 +21,8 @@ public class ReportResource {
         return Response.ok(reportService.getReport()).build();
     }
 
-    @GET
-    @Path("/get_request")
+    @POST
+    @Path("/download")
     public Response getDownloadFile() throws IOException {
         reportService.getXlsFile(reportService.getReport());
         File xls = new File("temp.xlsx");
